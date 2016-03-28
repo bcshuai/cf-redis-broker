d330b3b8a026df2c76bfe01b3ee5d6ceac31a49c
me020523 <me020523@gmail.com>
2016-03-28 13:55:39 +0800

    add two interfaces: IMetadataProvider and IMetadata

--- broker/bluemix-broker.go
+++ broker/bluemix-broker.go
@@ -1,42 +1,19 @@
 package broker
 
 import (
-	"errors"
-
 	"github.com/bcshuai/brokerapi"
-	"github.com/bcshuai/cf-redis-broker/brokerconfig"
 )
 
-const (
-	PlanNameShared    = "shared-vm"
-	PlanNameDedicated = "dedicated-vm"
-)
-
-type InstanceCredentials struct {
-	Host     string
-	Port     int
-	Password string
+type BluemixServiceMetadata struct {
+	brokerapi.ServiceMetadata
+	ServiceKeysSupported 	bool 	`json:serviceKeysSupported`
 }
 
-type InstanceCreator interface {
-	Create(instanceID string) error
-	Destroy(instanceID string) error
-	InstanceExists(instanceID string) (bool, error)
+type BluemixRedisServiceBroker struct {
+	RedisServiceBroker
 }
 
-type InstanceBinder interface {
-	Bind(instanceID string, bindingID string) (InstanceCredentials, error)
-	Unbind(instanceID string, bindingID string) error
-	InstanceExists(instanceID string) (bool, error)
-}
-
-type RedisServiceBroker struct {
-	InstanceCreators map[string]InstanceCreator
-	InstanceBinders  map[string]InstanceBinder
-	Config           brokerconfig.Config
-}
-
-func (redisServiceBroker *RedisServiceBroker) Services() []brokerapi.IMetadataProvider {
+func (broker *BluemixRedisServiceBroker) Services() []brokerapi.IMetadataProvider {
 	planList := []brokerapi.ServicePlan{}
 	for _, plan := range redisServiceBroker.plans() {
 		planList = append(planList, *plan)
@@ -44,12 +21,12 @@ func (redisServiceBroker *RedisServiceBroker) Services() []brokerapi.IMetadataPr
 
 	return []brokerapi.IMetadataProvider{
 		brokerapi.Service{
-			ID:          redisServiceBroker.Config.RedisConfiguration.ServiceID,
-			Name:        redisServiceBroker.Config.RedisConfiguration.ServiceName,
+			ID:          broker.Config.RedisConfiguration.ServiceID,
+			Name:        broker.Config.RedisConfiguration.ServiceName,
 			Description: "Redis service to provide a key-value store",
 			Bindable:    true,
 			Plans:       planList,
-			Metadata: brokerapi.ServiceMetadata{
+			Metadata: BluemixServiceMetadata{
 				DisplayName:      "Redis",
 				LongDescription:  "",
 				DocumentationUrl: "http://docs.pivotal.io/p1-services/Redis.html",
@@ -70,150 +47,4 @@ func (redisServiceBroker *RedisServiceBroker) Services() []brokerapi.IMetadataPr
 	}
 }
 
-func (redisServiceBroker *RedisServiceBroker) Provision(instanceID string, details brokerapi.ProvisionDetails, asyncAllowed bool) (brokerapi.ProvisionedServiceSpec, error){
-	
-	spec := brokerapi.ProvisionedServiceSpec{
-		false,
-		"",
-	}
 
-	if redisServiceBroker.instanceExists(instanceID) {
-		return spec, brokerapi.ErrInstanceAlreadyExists
-	}
-
-	if details.PlanID == "" {
-		return spec, errors.New("plan_id required")
-	}
-
-	planIdentifier := ""
-	for key, plan := range redisServiceBroker.plans() {
-		if plan.ID == details.PlanID {
-			planIdentifier = key
-			break
-		}
-	}
-
-	if planIdentifier == "" {
-		return spec, errors.New("plan_id not recognized")
-	}
-
-	instanceCreator, ok := redisServiceBroker.InstanceCreators[planIdentifier]
-	if !ok {
-		return spec, errors.New("instance creator not found for plan")
-	}
-
-	return spec, instanceCreator.Create(instanceID)
-}
-
-func (redisServiceBroker *RedisServiceBroker) Deprovision(instanceID string, details brokerapi.DeprovisionDetails, asyncAllowed bool) (brokerapi.IsAsync, error){
-	for _, instanceCreator := range redisServiceBroker.InstanceCreators {
-		instanceExists, _ := instanceCreator.InstanceExists(instanceID)
-		if instanceExists {
-			return false,instanceCreator.Destroy(instanceID)
-		}
-	}
-	return false,brokerapi.ErrInstanceDoesNotExist
-}
-
-func (redisServiceBroker *RedisServiceBroker) Bind(instanceID, bindingID string, details brokerapi.BindDetails) (brokerapi.Binding, error){
-	for _, repo := range redisServiceBroker.InstanceBinders {
-		instanceExists, _ := repo.InstanceExists(instanceID)
-		if instanceExists {
-			instanceCredentials, err := repo.Bind(instanceID, bindingID)
-			if err != nil {
-				return brokerapi.Binding{
-					nil,
-					"",
-					},  err
-			}
-			credentialsMap := map[string]interface{}{
-				"host":     instanceCredentials.Host,
-				"port":     instanceCredentials.Port,
-				"password": instanceCredentials.Password,
-			}
-			return brokerapi.Binding{
-					credentialsMap,
-					"",
-				}, nil
-		}
-	}
-
-	return brokerapi.Binding{
-				nil,
-				"",
-			},brokerapi.ErrInstanceDoesNotExist
-}
-
-func (redisServiceBroker *RedisServiceBroker) Unbind(instanceID, bindingID string, details brokerapi.UnbindDetails) error {
-	for _, repo := range redisServiceBroker.InstanceBinders {
-		instanceExists, _ := repo.InstanceExists(instanceID)
-		if instanceExists {
-			err := repo.Unbind(instanceID, bindingID)
-			if err != nil {
-				return brokerapi.ErrBindingDoesNotExist
-			}
-			return nil
-		}
-	}
-
-	return brokerapi.ErrInstanceDoesNotExist
-}
-
-func (redisServiceBroker *RedisServiceBroker) Update(instanceID string, details brokerapi.UpdateDetails, asyncAllowed bool) (brokerapi.IsAsync, error){
-	return false, nil
-}
-
-func (redisServiceBroker *RedisServiceBroker) LastOperation(instanceID string) (brokerapi.LastOperation, error){
-	return brokerapi.LastOperation{
-			brokerapi.Succeeded,
-			"done",
-		}, nil
-}
-
-func (redisServiceBroker *RedisServiceBroker) plans() map[string]*brokerapi.ServicePlan {
-	plans := map[string]*brokerapi.ServicePlan{}
-
-	if redisServiceBroker.Config.SharedEnabled() {
-		plans["shared"] = &brokerapi.ServicePlan{
-			ID:          redisServiceBroker.Config.RedisConfiguration.SharedVMPlanID,
-			Name:        PlanNameShared,
-			Description: "This plan provides a single Redis process on a shared VM, which is suitable for development and testing workloads",
-			Metadata: brokerapi.ServicePlanMetadata{
-				Bullets: []string{
-					"Each instance shares the same VM",
-					"Single dedicated Redis process",
-					"Suitable for development & testing workloads",
-				},
-				DisplayName: "Shared-VM",
-			},
-		}
-	}
-
-	if redisServiceBroker.Config.DedicatedEnabled() {
-		plans["dedicated"] = &brokerapi.ServicePlan{
-			ID:          redisServiceBroker.Config.RedisConfiguration.DedicatedVMPlanID,
-			Name:        PlanNameDedicated,
-			Description: "This plan provides a single Redis process on a dedicated VM, which is suitable for production workloads",
-			Metadata: brokerapi.ServicePlanMetadata{
-				Bullets: []string{
-					"Dedicated VM per instance",
-					"Single dedicated Redis process",
-					"Suitable for production workloads",
-				},
-				DisplayName: "Dedicated-VM",
-			},
-		}
-	}
-
-	return plans
-}
-
-func (redisServiceBroker *RedisServiceBroker) instanceExists(instanceID string) bool {
-	for _, instanceCreator := range redisServiceBroker.InstanceCreators {
-		instanceExists, _ := instanceCreator.InstanceExists(instanceID)
-		if instanceExists {
-			return true
-		}
-	}
-	return false
-}
